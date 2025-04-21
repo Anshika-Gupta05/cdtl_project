ee2d31c2693fdd44dcb9d3c082c588170449d6c0
Anshika-Gupta05 <anshika.gupta2021@vitstudent.ac.in>
2025-04-21 21:58:16 +0530

    first commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,10 +3,13 @@ export const uploadImage = async (file) => {
   formData.append("image", file);
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/predict", {
-      method: "POST",
-      body: formData,
-    });
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/predict`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Network response was not ok");
